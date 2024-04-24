8f68dd1fbbbae4cb853ee58e6f3924b79166727c
AYB01 <ayoubboulidam07@gmail.com>
2024-04-24 16:59:55 +0100

    Server Sockets

--- src/main/java/ayb/JAVAProjects/httpServer/HttpServer.java
+++ src/main/java/ayb/JAVAProjects/httpServer/HttpServer.java
@@ -1,14 +1,11 @@
 package ayb.JAVAProjects.httpServer;
 
-
 /*
-*
-*Driver Class for the Http Server
-* */
+ * Driver Class for the Http Server
+ */
 
 import ayb.JAVAProjects.httpServer.config.Configuration;
 import ayb.JAVAProjects.httpServer.config.ConfigurationManager;
-
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,45 +17,56 @@ public class HttpServer {
 
     public static void main(String[] args) {
 
+        // Print a message indicating that the server is starting
         System.out.println("Server starting ...");
+
+        // Load the configuration file using the ConfigurationManager singleton
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
+        // Retrieve the current configuration
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        // Print the port and web root directory being used
         System.out.println("Using Port : " + conf.getPort());
         System.out.println("Using WebRoot  : " + conf.getWebroot());
 
-
         try {
+            // Create a ServerSocket to listen for incoming connections on the specified port
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
 
+            // Accept incoming connection requests and create a socket for each client
+            Socket socket = serverSocket.accept();
 
-        ServerSocket serverSocket = new ServerSocket(conf.getPort());
-        Socket socket = serverSocket.accept();
+            // Get input and output streams for the socket
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
 
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream=socket.getOutputStream();
-
-        // page to send to browser
-
+            // HTML page to be sent to the browser
             String html = "<!DOCTYPE html><html><head><title>Simple Java Http Server</title></head><body><h1>Served Using Our  Http Server</h1></body></html>";
 
-            final  String CRFL = "\r\n"; // 13 , 10
+            // Carriage return and line feed characters
+            final String CRFL = "\r\n"; // 13 , 10
+
+            // HTTP response to be sent back to the client
             String response =
-                    "HTTP/1.1 200 OK" +CRFL+ // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE :
-                     "Content-Length :"+ html.getBytes().length+CRFL+ // Header
-                            CRFL+
-                            html+
-                            CRFL+CRFL;
+                    "HTTP/1.1 200 OK" + CRFL + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE :
+                            "Content-Length :" + html.getBytes().length + CRFL + // Header
+                            CRFL +
+                            html +
+                            CRFL + CRFL;
 
-         outputStream.write(response.getBytes());
+            // Write the HTTP response to the output stream
+            outputStream.write(response.getBytes());
 
-        inputStream.close();
-        outputStream.close();
-        socket.close();
-        serverSocket.close();
+            // Close input and output streams and the socket
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
 
         } catch (IOException e) {
+            // Print any IOExceptions that occur during server operation
             e.printStackTrace();
         }
-
-
     }
 }
